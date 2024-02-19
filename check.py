dc1478c27c4759a4877ad2523c444f524eb303a4
gatoora <mm.pourdad@gmail.com>
2024-02-19 16:28:55 +0330

    done functional

---
+++ check.py
@@ -0,0 +1,4 @@
+word = "hello"
+letter = "l"
+indices = [i for i, char in enumerate(word) if char == letter]
+print(indices[0])
--- hangman-func.py
+++ hangman-func.py
@@ -1,20 +1,68 @@
-dictlist = []
-dictlist = "aliakbar", "asghar"
-
-print(len(dictlist[0]))
-guess = len(dictlist[0])
-stars = ""
-for i in range(guess):
-    stars += "*"
-
-x0 = "b"
-xx = dictlist[0].index(x0)
-starlist = list(stars)
-starlist[xx] = x0
-starsstr = "".join(starlist)
+import random
+wordlist = []
+wordlist = "aliakbar", "asghar", "mehdi"
+word = ""
 
 
-# x0 = input()
-# xx = dictlist[0].index(x0)
-# if xx != None:
-#     stars.index[xx] = x0
+def word_chooser():
+    return random.choice(wordlist)
+    # print(word)
+
+
+word = word_chooser()
+
+print(word)
+
+
+def starmaker():
+    stars = ""
+    wordlen = len(word)
+    for i in range(wordlen):
+        stars += "*"
+    return stars
+
+
+xstar = starmaker()
+guess_remain = 8
+
+
+def game_status():
+    if guess_remain == 0:
+        print("you loose")
+    else:
+        if "*" not in lstar:
+            print("you won")
+            exit()
+
+
+lstar = list(xstar)
+for bb in range(guess_remain):
+    x = input("guess: ")
+    indices = [i for i, char in enumerate(word) if char == x]
+    if indices == []:
+        guess_remain -= 1
+        game_status()
+        print(guess_remain)
+    for bb in indices:
+        lstar[bb] = x
+    print(lstar)
+
+    game_status()
+    # if "*" not in lstar:
+    #     print("you won")
+
+
+# starsstr = ""
+# starlist = list(stars)
+# for w in range(guess_remain):
+#     x = input("Guess a letter: ")
+#     if x in list(dictlist[0]):
+#         indices = [i for i, char in enumerate(dictlist[0]) if char == x]
+#         lenind= iny(len(indices))
+#         for b in range(lenind):
+
+#         xxx = dictlist[0].index(x)
+
+#         starlist[xxx] = x
+#         starsstr = "".join(starlist)
+#         print(starsstr)
